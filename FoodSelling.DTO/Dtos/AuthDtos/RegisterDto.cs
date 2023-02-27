20f4acb0170200b10c5ea509c8e30bebdf72882f
ImViet <dinhquocviet318@gmail.com>
2023-02-27 23:43:50 +0700

    Create dto

---
+++ FoodSelling.DTO/Dtos/AuthDtos/RegisterDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FoodSelling.DTO.Dtos.AuthDtos
+{
+    public class RegisterDto
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public int IdentityCard { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ FoodSelling.DTO/Dtos/RatingDtos/CreateRatingDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FoodSelling.DTO.Dtos.RatingDtos
+{
+    public class CreateRatingDto
+    {
+        public int ProductId { get; set; }
+        public string UserName { get; set; }
+        public int Star { get; set; }
+        public string? Comment { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+    }
+}
