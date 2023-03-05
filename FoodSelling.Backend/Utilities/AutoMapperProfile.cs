4a9c44a44074dbfed4bb76c47352dbce47e219d4
ImViet <dinhquocviet318@gmail.com>
2023-03-05 12:30:44 +0700

    Update BE Category

--- FoodSelling.Backend/Utilities/AutoMapperProfile.cs
+++ FoodSelling.Backend/Utilities/AutoMapperProfile.cs
@@ -1,11 +1,14 @@
 ﻿using AutoMapper;
 using FoodSelling.Backend.Entities;
 using FoodSelling.DTO.Dtos.AuthDtos;
+using FoodSelling.DTO.Dtos.CustomerSite.CategoryDtos;
+using FoodSelling.DTO.Dtos.CustomerSite.ProductDtos;
+using FoodSelling.DTO.Dtos.RatingDtos;
 using System.Runtime.InteropServices;
 
 namespace FoodSelling.Backend.Utilities
 {
-    public class AutoMapperProfile: Profile
+    public class AutoMapperProfile : Profile
     {
         public AutoMapperProfile()
         {
@@ -22,6 +25,12 @@ namespace FoodSelling.Backend.Utilities
         {
             CreateMap<User, AccountDto>()
                 .ForMember(d => d.Token, t => t.Ignore());
+            CreateMap<Category, CategoryDto>()
+                .ForMember(d => d.Products, t => t.MapFrom(src => src.Products));
+            CreateMap<Product, ProductDto>()
+                .ForMember(d => d.CategoryName, t => t.MapFrom(src => src.Category.CategoryName))
+                .ForMember(d => d.Ratings, t => t.MapFrom(src => src.Ratings));
+            CreateMap<Rating, RatingDto>();
         }
     }
 }
